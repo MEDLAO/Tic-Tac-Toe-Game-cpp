668ba3a8a4c88068e0c8743850bff2a8febf32d9
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-04-01 17:24:32 +0200

    creating files and adding libraries

---
+++ Tic-Tac-Toe-Game-C++/tictactoe.cpp
@@ -0,0 +1,8 @@
+//
+//  tictactoe.cpp
+//  Tic-Tac-Toe-Game-C++
+//
+//  Created by Sofiane Medhouny Laouina on 01/04/2024.
+//
+
+#include "tictactoe.hpp"
---
+++ Tic-Tac-Toe-Game-C++/tictactoe.hpp
@@ -0,0 +1,13 @@
+//
+//  tictactoe.hpp
+//  Tic-Tac-Toe-Game-C++
+//
+//  Created by Sofiane Medhouny Laouina on 01/04/2024.
+//
+
+#ifndef tictactoe_hpp
+#define tictactoe_hpp
+
+#include <stdio.h>
+
+#endif /* tictactoe_hpp */
