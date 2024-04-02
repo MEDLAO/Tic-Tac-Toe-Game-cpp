97805ccc896715a57ff18ad86bcf2c4cf7d24f39
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-04-02 18:31:37 +0200

    start implementing introduction() function

--- Tic-Tac-Toe-Game-C++/tictactoe.cpp
+++ Tic-Tac-Toe-Game-C++/tictactoe.cpp
@@ -7,5 +7,25 @@
 
 #include <iostream>
 #include "tictactoe.hpp"
-
+// globally defined
 using namespace std;
+string board[9] = {"", "", "", "", "", "", "", "", ""};
+int player = 1;
+int position = 0;
+
+void introduction()
+{
+    cout << "Press [Enter] to begin: ";
+    getchar(); // haults the screen
+    cout << "\n";
+    
+    cout << "***********\n";
+    cout << "Tic-Tac-Toe\n";
+    cout << "***********\n";
+    
+    cout << "Player 1)  X\n";
+    cout << "Player 2)  O\n";
+    
+    cout << "The 3x3 grid is shown below:\n\n";
+    
+}
